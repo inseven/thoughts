a5eb87a421dcbed2f617262f93268e3dcbf0d49d
Jason Morley <hello@jbmorley.co.uk>
2024-07-07 14:04:21 -1000

    chore: Add Developer ID profile (#110)
    This change also moves profiles into a subdirectory.

--- scripts/build.sh
+++ scripts/build.sh
@@ -127,7 +127,8 @@ echo "$APPLE_DISTRIBUTION_CERTIFICATE_PASSWORD" | build-tools import-base64-cert
 echo "$MACOS_DEVELOPER_INSTALLER_CERTIFICATE_PASSWORD" | build-tools import-base64-certificate --password "$KEYCHAIN_PATH" "$MACOS_DEVELOPER_INSTALLER_CERTIFICATE_BASE64"
 
 # Install the provisioning profiles.
-build-tools install-provisioning-profile "Thoughts_Mac_App_Store_Profile.provisionprofile"
+build-tools install-provisioning-profile "profiles/Thoughts_Developer_ID_Profile.provisionprofile"
+build-tools install-provisioning-profile "profiles/Thoughts_Mac_App_Store_Profile.provisionprofile"
 
 # Build and archive the macOS project.
 sudo xcode-select --switch "$MACOS_XCODE_PATH"
