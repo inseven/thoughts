852a106dd4ff60a98ec969470457e6d2e38a18e1
Jason Morley <hello@jbmorley.co.uk>
2025-07-17 18:32:07 -1000

    Don't copy the app store package for the regular build

--- scripts/build.sh
+++ scripts/build.sh
@@ -143,9 +143,6 @@ build-tools install-provisioning-profile "profiles/Thoughts_Mac_App_Store_Profil
 
 ## Developer ID Build
 
-# Copy the App Store Package.swift configuration.
-cp ThoughtsCore/Package-App-Store.swift ThoughtsCore/Package.swift
-
 # Build and archive the macOS project.
 sudo xcode-select --switch "$MACOS_XCODE_PATH"
 xcodebuild \
