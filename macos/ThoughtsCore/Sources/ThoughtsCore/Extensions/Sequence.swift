a65d7d6f7672fe4c8205086e62b79f8075b79059
Jason Morley <hello@jbmorley.co.uk>
2026-06-04 14:35:23 +0900

    refactor: Move `Sequence` into ThoughtsCore

--- macos/ThoughtsCore/Sources/ThoughtsCore/Extensions/Sequence.swift
+++ macos/ThoughtsCore/Sources/ThoughtsCore/Extensions/Sequence.swift
@@ -36,8 +36,8 @@ private let htmlRegex = try! NSRegularExpression(
 )
 
 #if os(macOS)
-let defaultEditorFont = NSFont.monospacedSystemFont(ofSize: 14.0, weight: .regular)
-let codeFont = NSFont.monospacedSystemFont(ofSize: 14.0, weight: .thin)
+nonisolated(unsafe) let defaultEditorFont = NSFont.monospacedSystemFont(ofSize: 14.0, weight: .regular)
+nonisolated(unsafe) let codeFont = NSFont.monospacedSystemFont(ofSize: 14.0, weight: .thin)
 let headingTraits: NSFontDescriptor.SymbolicTraits = [.bold, .expanded]
 let boldTraits: NSFontDescriptor.SymbolicTraits = [.bold]
 let emphasisTraits: NSFontDescriptor.SymbolicTraits = [.italic]
@@ -79,7 +79,7 @@ public extension Sequence where Iterator.Element == HighlightRule {
                 pattern: linkOrImageRegex,
                 formattingRules: [
                     TextFormattingRule(fontTraits: boldTraits),
-                    TextFormattingRule(key: .foregroundColor, value: NSColor.systemBlue),
+                    TextFormattingRule(key: .foregroundColor, value: Color.accentColor),
                 ]
             ),
             HighlightRule(
