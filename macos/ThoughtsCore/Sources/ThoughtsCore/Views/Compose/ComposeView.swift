10d28f38b835041e82f49fed6ecfe6fd15fdd81d
Jason Morley <hello@jbmorley.co.uk>
2026-07-02 10:37:54 -1000

    fix: 📕 Enable continuous spell checking (#205)

--- macos/ThoughtsCore/Sources/ThoughtsCore/Views/Compose/ComposeView.swift
+++ macos/ThoughtsCore/Sources/ThoughtsCore/Views/Compose/ComposeView.swift
@@ -53,6 +53,13 @@ struct ComposeView: View {
         @Bindable var applicationModel = applicationModel
         VStack(spacing: 0) {
             HighlightedTextEditor(text: $applicationModel.document.content, highlightRules: .thoughtsMarkdown)
+                .introspect { internals in
+#if os(macOS)
+                    internals.textView.isContinuousSpellCheckingEnabled = true
+#else
+                    internals.textView.spellCheckingType = .yes
+#endif
+                }
                 .frame(minWidth: 400)
                 .edgesIgnoringSafeArea(.all)
                 .focused($focus, equals: .text)
